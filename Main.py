49579d430e24d6146b18de08da97b58401295dd4
dmccolly <128931847+dmccolly@users.noreply.github.com>
2025-08-13 17:16:06 -0600

    Update Main.py

--- Main.py
+++ Main.py
@@ -1,11 +1,20 @@
-from flask import Flask
+from flask import Flask, render_template
 from flask_cors import CORS
 from routes.webflow_proxy import webflow_proxy_blueprint
 import os
 
 app = Flask(__name__)
-CORS(app) # Allow all origins
+CORS(app)  # Allow all origins
+
 app.register_blueprint(webflow_proxy_blueprint)
+
+@app.route('/voxpro-manager')
+def voxpro_manager():
+    return render_template('voxpro-manager.html')
+
+@app.route('/')
+def home():
+    return "VoxPro Backend API"
 
 if __name__ == '__main__':
     port = int(os.environ.get('PORT', 5001))
