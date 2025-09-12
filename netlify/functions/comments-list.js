e1f9ec60ce30b61d202dadbf78afc38de05690e1
dmccolly <128931847+dmccolly@users.noreply.github.com>
2025-09-12 15:45:34 -0600

    Update comments-list.js

--- netlify/functions/comments-list.js
+++ netlify/functions/comments-list.js
@@ -1,7 +1,16 @@
 const fetch = global.fetch || ((...args) => import('node-fetch').then(({ default: f }) => f(...args)));
 
 exports.handler = async (event) => {
-  if (event.httpMethod === "OPTIONS" ) return { statusCode: 200 };
+  if (event.httpMethod === "OPTIONS") {
+    return {
+      statusCode: 200,
+      headers: {
+        'Access-Control-Allow-Origin': process.env.ALLOW_ORIGINS || process.env.ALLOW_ORIGIN || '*',
+        'Access-Control-Allow-Headers': 'Content-Type, Authorization',
+        'Access-Control-Allow-Methods': 'GET, OPTIONS',
+      }
+    };
+  }
   
   const headers = {
     'Access-Control-Allow-Origin': process.env.ALLOW_ORIGINS || process.env.ALLOW_ORIGIN || '*',
@@ -20,7 +29,7 @@ exports.handler = async (event) => {
   try {
     const authHeaders = { Authorization: `Bearer ${process.env.WEBFLOW_API_TOKEN}` };
     const url = `https://api.webflow.com/v2/collections/${process.env.COMMENTS_COLLECTION_ID}/items?limit=100`;
-    const res = await fetch(url, { headers: authHeaders } );
+    const res = await fetch(url, { headers: authHeaders });
     const data = await res.json();
     const items = (data.items || []).filter(i =>
       i.fieldData.approved === true &&
@@ -45,3 +54,4 @@ exports.handler = async (event) => {
     };
   }
 };
+
